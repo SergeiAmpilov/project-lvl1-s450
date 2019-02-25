b587fbc0449ea0f80c2b01afc0ec7cc2e84c8f8d
Sergei Ampilov <sergei.ampilov@gmail.com>
2019-02-25 15:45:28 +0300

    версия 1.0.1 - запрашивает имя

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import readlineSync from 'readline-sync';
+
+const greeting = () => {
+  console.log('Welcome to the Brain Games!');
+
+  const actual = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${actual}!`);
+}
+
+export default greeting;
